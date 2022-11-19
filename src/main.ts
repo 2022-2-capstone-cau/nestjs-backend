9d37de4d3b6fd1ef86f2845f126f8cf1d372599e
yongsoocho <yongsoocho578@gmail.com>
2022-11-19 17:25:44 +0900

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,23 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./module/app.module";
-import { SuccessResponse } from "./common/interceptors/SuccessResponse.interceptor";
 import { GlobalExceptionFilter } from "./common/exceptions/Global.exception.filter";
 import { ErrorResponseInterceptor } from "./common/interceptors/ErrorResponse.interceptor";
 import { ValidationPipe } from "@nestjs/common";
+import * as morgan from "morgan";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	app.enableCors({
+		origin: "*",
+		credentials: true,
+	});
+	app.use(morgan("combined"));
 
 	app.useGlobalPipes(new ValidationPipe());
 	app.useGlobalFilters(new GlobalExceptionFilter());
 	app.useGlobalInterceptors(new ErrorResponseInterceptor());
 	// app.useGlobalInterceptors(new SuccessResponse());
 
-	await app.listen(80);
+	await app.listen(3000);
 }
 bootstrap();
