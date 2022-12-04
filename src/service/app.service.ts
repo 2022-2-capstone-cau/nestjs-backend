1cc44ad06fb2bc59b8d07e8b69889e5f3415939b
yongsoocho <yongsoocho578@gmail.com>
2022-12-04 16:54:56 +0900

    userLib

--- src/service/app.service.ts
+++ src/service/app.service.ts
@@ -12,10 +12,39 @@ export class AppService {
 	}
 
 	async updateAll() {
+		const newCategory = await this.prisma.category.findUnique({
+			where: { category: "IT" },
+		});
+		if (!newCategory) {
+			await this.prisma.category.create({
+				data: { category: "IT" },
+			});
+		}
 		const category = await this.prisma.category.findMany({});
+		const query = await this.prisma.$queryRaw`
+			SELECT *
+			FROM "category" AS c
+			GROUP BY c.category
+		`;
+		await this.prisma.userLib.upsert({
+			where: { user_id: 1 },
+			update: { best_category: "IT" },
+			create: { best_category: "IT", user_id: 1 },
+		});
+		await this.prisma.userLib.upsert({
+			where: { user_id: 2 },
+			update: { best_category: "IT" },
+			create: { best_category: "IT", user_id: 2 },
+		});
+		await this.prisma.userLib.upsert({
+			where: { user_id: 3 },
+			update: { best_category: "IT" },
+			create: { best_category: "IT", user_id: 3 },
+		});
 
 		return {
 			category,
+			query,
 		};
 	}
 
