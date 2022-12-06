8345c5de4e3fa358f56a4cfe7adddf1c01160b70
yongsoocho <yongsoocho578@gmail.com>
2022-12-06 11:21:14 +0900

    user lib

--- src/service/app.service.ts
+++ src/service/app.service.ts
@@ -12,37 +12,15 @@ export class AppService {
 	}
 
 	async updateAll() {
-		const r1 = [];
-		const r2 = [];
-		for (let i = 6; i < 40; i++) {
-			if (i % 2 === 0) r1.push(i);
-			if (i % 2 === 1) r2.push(i);
-		}
-		const result1 = await this.prisma.book.updateMany({
-			where: {
-				book_id: {
-					in: r1,
-				},
-			},
-			data: {
-				user_id: 2,
-			},
-		});
-		const result2 = await this.prisma.book.updateMany({
-			where: {
-				book_id: {
-					in: r2,
-				},
-			},
-			data: {
-				user_id: 3,
-			},
-		});
-
-		return {
-			result1,
-			result2,
-		};
+		// const xx1 = await this.prisma.userLib.findUnique({ where: { user_id: 1 } });
+		// const xx2 = await this.prisma.userLib.findUnique({ where: { user_id: 2 } });
+		// const xx3 = await this.prisma.userLib.findUnique({ where: { user_id: 3 } });
+		//
+		// if (!xx1) await this.prisma.userLib.create({ data: { user_id: 1, best_category: "IT" } });
+		// if (!xx2) await this.prisma.userLib.create({ data: { user_id: 1, best_category: "IT" } });
+		// if (!xx3) await this.prisma.userLib.create({ data: { user_id: 1, best_category: "IT" } });
+		//
+		return await this.prisma.userLib.findMany({});
 	}
 
 	test() {
